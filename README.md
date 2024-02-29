08080f89cfc2ecc947f9326531bd980b9200dd21
Refo <refotri2001@gmail.com>
2024-02-29 22:37:56 +0700

    added success message on added film

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # aldmictest
 
-A new Flutter project.
+Untuk login dalam aplikasi ini saya menggunakan email dan password : 
+
+email : aldmic@gmail.com
+password : 123abc123
+
+Berikut adalah spesifikasi dari sistem untuk tugas ini. 
+
+1. Design pattern yang digunakan adalah MVC (Model View Controller)
+2. State Management yang dipilih adalah GetX 
+3. Penyimpanan data ada pada Firebase Firestore
+4. Authentikasi pengguna menggunakan bantuan Firebase Authentication
+5. library yang digunakan pada project ini antara lain : 
+a. cloud_firestore
+b. firebase_auth
+c. firebase_core
+d. flutter_native_splash
+e. flutter_spinkit
+f. get
+g. http
+h. ionicons
+i. lottie
 
 ## Getting Started
 
--- lib/controller/moviedetailspagecontroller.dart
+++ lib/controller/moviedetailspagecontroller.dart
@@ -3,6 +3,8 @@ import 'package:aldmictest/model/moviedetails.dart';
 import 'package:aldmictest/secrets/apikey.dart';
 import 'package:aldmictest/services/authenticationservice.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:http/http.dart' as http;
 import 'package:get/get.dart';
 
@@ -40,26 +42,56 @@ class MovieDetailsPageController extends GetxController {
   }
 
   Future<void> saveFavoriteMovie(MovieDetails movieDetails) async {
-    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-        .collection('Users')
-        .doc(AuthenticationService.instance.authUser?.uid)
-        .collection('favorite_movies')
-        .where('id', isEqualTo: movieDetails.id)
-        .get();
-
-    if (querySnapshot.docs.isEmpty) {
-      FirebaseFirestore.instance
+    try {
+      openDialig();
+      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
           .collection('Users')
           .doc(AuthenticationService.instance.authUser?.uid)
           .collection('favorite_movies')
-          .doc(movieDetails.id)
-          .set({
-        'id': movieDetails.id,
-        'poster': movieDetails.poster,
-        'title': movieDetails.title,
-        'type': movieDetails.type,
-        'year': movieDetails.year,
-      });
+          .where('id', isEqualTo: movieDetails.id)
+          .get();
+
+      if (querySnapshot.docs.isEmpty) {
+        FirebaseFirestore.instance
+            .collection('Users')
+            .doc(AuthenticationService.instance.authUser?.uid)
+            .collection('favorite_movies')
+            .doc(movieDetails.id)
+            .set({
+          'id': movieDetails.id,
+          'poster': movieDetails.poster,
+          'title': movieDetails.title,
+          'type': movieDetails.type,
+          'year': movieDetails.year,
+        });
+      }
+    } catch (e) {
+      Get.back();
+      throw 'Something went wrong';
+    } finally {
+      Get.back();
+      Get.snackbar("Sukses", "Film berhasil ditambahkan!",
+          backgroundColor: const Color.fromARGB(255, 1, 114, 26),
+          colorText: Colors.white);
     }
+  }
+
+  void openDialig() {
+    Get.dialog(
+        Dialog(
+          backgroundColor: Colors.blue,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: const SizedBox(
+            width: 100,
+            height: 150,
+            child: SpinKitCubeGrid(
+              size: 100,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        barrierDismissible: false);
   }
 }
